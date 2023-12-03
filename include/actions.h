7826d5e8166773bffe9a8ee70be135f1736055f3
Scarface1809 <joao.miguel.santos1809@gmail.com>
2023-12-03 19:46:33 +0000

    Commit

--- include/actions.h
+++ include/actions.h
@@ -1,14 +1,31 @@
 #ifndef ACTIONS_H
 #define ACTIONS_H
 
+#define UID_SIZE 6
+#define PASS_SIZE 8
+
+typedef struct User
+{
+    char uid[UID_SIZE + 1];
+    char password[PASS_SIZE + 1];
+} User;
+
+typedef struct Auction
+{
+    char name[11]; // Define 11
+    // asset(image)
+    float start_value;
+    // Time duration
+} Auction;
+
 // Verifications
-int verify_user_credentials(char buffer[]);
+int verify_user_credentials(User user);
 int verify_UID(char buffer[]);
 int verify_AID(char buffer[]);
 // UDP
 int login_user(char buffer[]);
 int logout_user(char buffer[]);
-//int unregister_user(char buffer[]);
+// int unregister_user(char buffer[]);
 int myactions_user(char buffer[]);
 int mybids_user(char buffer[]);
 int list_user(char buffer[]);
@@ -19,7 +36,27 @@ int open(char buffer[]);
 int close(char buffer[]);
 int show_asset(char buffer[]);
 int bid(char buffer[]);
-// LOCAL
-int exit(char buffer[]);
 */
+// LOCAL
+// int exit(char buffer[]);
+// Server
+//  Users
+void create_user_folder(User user);
+void create_pass_file(User user);
+void create_login_file(User user);
+void create_hosted_folder(User user);
+void create_bidded_folder(User user);
+// void create_hosted_auction_file(User user, Auction auc);
+// void create_bidded_auction_file(User user, Auction auc);
+//   Auctions
+/*
+void create_auction_folder(Auction auc);
+void create_start_file(Auction auc);
+void create_asset(Auction auc);
+void create_end_file(Auction auc);
+void create_
+ */
+// LookUps
+
+/**/
 #endif
--- src/actions.c
+++ src/actions.c
@@ -2,28 +2,37 @@
 #include "string.h"
 #include "stdio.h"
 #include "ctype.h"
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <errno.h>
 
-int verify_user_credentials(char buffer[])
+User parse_user_credentials(char buffer[])
 {
+    User user;
+    sscanf(buffer, "%*s\t%s\t%s", user.uid, user.password);
+    return user;
+}
 
-    char command[20], uid[8], password[10];
-    sscanf(buffer, "%s\t%s\t%s", command, uid, password);
-    if (strlen(uid) != 6 || strlen(password) != 8)
+int verify_user_credentials(User user)
+{
+    if (strlen(user.uid) != UID_SIZE || strlen(user.password) != PASS_SIZE)
     {
         printf("Invalid input: uid or password size wrong\n");
         return 0;
     }
-    for (int i = 0; i < strlen(uid); i++)
+    for (int i = 0; i < strlen(user.uid); i++)
     {
-        if (!isdigit(uid[i]))
+        if (!isdigit(user.uid[i]))
         {
             printf("Invalid input: uid\n");
             return 0;
         }
     }
-    for (int i = 0; i < strlen(password); i++)
+    for (int i = 0; i < strlen(user.password); i++)
     {
-        if (!isdigit(password[i]) && !isalpha(password[i]))
+        if (!isdigit(user.password[i]) && !isalpha(user.password[i]))
         {
             printf("Invalid input: password\n");
             return 0;
@@ -37,7 +46,7 @@ int verify_UID(char buffer[])
 {
     char command[20], uid[8];
     sscanf(buffer, "%s\t%s", command, uid);
-    if (strlen(uid) != 6)
+    if (strlen(uid) != UID_SIZE)
     {
         printf("Invalid input: uid or password size wrong\n");
         return 0;
@@ -75,18 +84,22 @@ int verify_AID(char buffer[])
 
 int login_user(char buffer[])
 {
-    return verify_user_credentials(buffer);
+    User user = parse_user_credentials(buffer);
+    create_user_folder(user);
+    return verify_user_credentials(user);
 }
 
 int logout_user(char buffer[])
 {
-    return verify_UID(buffer);
+    User user = parse_user_credentials(buffer);
+    return verify_user_credentials(user);
 }
 
-/*int unregister_user(char buffer[])
+int unregister_user(char buffer[])
 {
-    return verify_user_credentials(buffer);
-}*/
+    User user = parse_user_credentials(buffer);
+    return verify_user_credentials(user);
+}
 
 int myactions_user(char buffer[])
 {
@@ -106,4 +119,87 @@ int list_user(char buffer[])
 int show_record_user(char buffer[])
 {
     return verify_AID(buffer);
+}
+
+void create_user_folder(User user)
+{
+    // create the User folder
+    char folder_Path[19];
+    sprintf(folder_Path, "ASDIR/USERS/%s", user.uid);
+    if (mkdir(folder_Path, 0777) == 0)
+    {
+        create_pass_file(user);
+        create_login_file(user);
+        create_hosted_folder(user);
+        create_bidded_folder(user);
+    }
+    else
+    {
+        perror("Error creating user folder");
+    }
+}
+
+void create_pass_file(User user)
+{
+    char pass_file_path[37];
+    // N sei se é necessário checkar se o folder do user existe primeiro
+    sprintf(pass_file_path, "ASDIR/USERS/%s/%s_pass.txt", user.uid, user.uid);
+
+    FILE *pass_file = fopen(pass_file_path, "w");
+
+    if (pass_file != NULL)
+    {
+        fprintf(pass_file, "%s", user.password);
+        fclose(pass_file);
+        // printf("Password file created successfully: %s\n", pass_file_path);
+    }
+    else
+    {
+        perror("Error creating password file");
+    }
+}
+
+void create_login_file(User user)
+{
+    char login_file_path[38];
+    // N sei se é necessário checkar se o folder do user existe primeiro
+    sprintf(login_file_path, "ASDIR/USERS/%s/%s_login.txt", user.uid, user.uid);
+
+    FILE *login_file = fopen(login_file_path, "w");
+
+    if (login_file != NULL)
+    {
+        fclose(login_file);
+        // printf("Login file created successfully: %s\n", login_file_path);
+    }
+    else
+    {
+        perror("Error creating login file");
+    }
+}
+
+void create_hosted_folder(User user)
+{
+    char hosted_folder_path[26];
+    // N sei se é necessário checkar se o folder do user existe primeiro
+    sprintf(hosted_folder_path, "ASDIR/USERS/%s/HOSTED", user.uid);
+    if (mkdir(hosted_folder_path, 0777))
+    {
+        printf("Created Hosted folder sucessfully");
+    }
+    else
+    {
+        perror("Error creating user folder");
+    }
+}
+
+void create_bidded_folder(User user)
+{
+    char bidded_folder_Path[26];
+    // N sei se é necessário checkar se o folder do user existe primeiro
+    sprintf(bidded_folder_Path, "ASDIR/USERS/%s/BIDDED", user.uid);
+    if (mkdir(bidded_folder_Path, 0777) == 0)
+    {
+        printf("test");
+    }
 }
